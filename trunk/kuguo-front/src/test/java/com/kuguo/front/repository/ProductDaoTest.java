814d42b3a6d67d76c5d548902cd48541045561de
985968223@qq.com <985968223@qq.com@2244cf6e-af35-60ac-9df1-d7610207b578>
2013-05-01 15:40:34 +0000

    Product 存储测试

--- trunk/kuguo-front/src/test/java/com/kuguo/front/repository/ProductDaoTest.java
+++ trunk/kuguo-front/src/test/java/com/kuguo/front/repository/ProductDaoTest.java
@@ -1,7 +1,5 @@
 package com.kuguo.front.repository;
 
-import java.util.List;
-
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
@@ -10,15 +8,29 @@ import org.springside.modules.test.spring.SpringTransactionalTestCase;
 import static junit.framework.Assert.*;
 
 import com.kuguo.front.entity.Product;
+
 @DirtiesContext
 @ContextConfiguration(locations = { "/applicationContext.xml" })
-public class ProductDaoTest extends SpringTransactionalTestCase{
+public class ProductDaoTest extends SpringTransactionalTestCase {
 	@Autowired
 	private ProductDao productDao;
-	
+
 	@Test
-	public void getProduct(){
+	public void getProduct() {
 		Product product = productDao.get(1L);
 		assertNotNull(product);
+	}
+
+	@Test
+	public void saveProduct() {
+		Product product = new Product();
+		product.setName("测试");
+		product.setPicture("http");
+		product.setOrder_link("http://sjfdlsjdfk");
+		product.setDescription("测试");
+		product.setBrand("");
+		product.setPrice(0);
+		product.setChannel("");
+		productDao.save(product);
 	}
 }
