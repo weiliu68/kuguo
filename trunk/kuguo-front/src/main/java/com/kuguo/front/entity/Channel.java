4168fca20d500d9cae2f6a7d3772a75d7096aa35
985968223@qq.com <985968223@qq.com@2244cf6e-af35-60ac-9df1-d7610207b578>
2013-05-02 04:03:00 +0000

    entity 完善

--- trunk/kuguo-front/src/main/java/com/kuguo/front/entity/Channel.java
+++ trunk/kuguo-front/src/main/java/com/kuguo/front/entity/Channel.java
@@ -1,13 +1,15 @@
 package com.kuguo.front.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "channel")
 public class Channel extends IdEntity {
 	private String name;
-	private User user_id;
+	private Channel channel;
 
 	public String getName() {
 		return name;
@@ -15,6 +17,16 @@ public class Channel extends IdEntity {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	@ManyToOne
+	@JoinColumn(name = "parent_id")
+	public Channel getChannel() {
+		return channel;
+	}
+
+	public void setChannel(Channel channel) {
+		this.channel = channel;
 	}
 
 }
