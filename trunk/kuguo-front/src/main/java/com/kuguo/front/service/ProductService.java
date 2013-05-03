abd8b69bf3aef152590f28e84f584abb68ea8832
weiliu68 <weiliu68@2244cf6e-af35-60ac-9df1-d7610207b578>
2013-05-03 08:08:36 +0000

    删除对springside依赖

--- trunk/kuguo-front/src/main/java/com/kuguo/front/service/ProductService.java
+++ trunk/kuguo-front/src/main/java/com/kuguo/front/service/ProductService.java
@@ -1,18 +1,13 @@
 package com.kuguo.front.service;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import org.springside.modules.persistence.DynamicSpecifications;
-import org.springside.modules.persistence.SearchFilter;
-import org.springside.modules.persistence.SearchFilter.Operator;
 
 import com.kuguo.front.entity.Product;
 import com.kuguo.front.repository.ProductDao;
@@ -22,9 +17,9 @@ import com.kuguo.front.utils.Constant;
 @Transactional(readOnly = true)
 public class ProductService {
 	private ProductDao productDao;
-	
-	public List<Product> getProductsByPage(int page){
-		return productDao.getAll(page*Constant.PAGE_NUMBER);
+
+	public List<Product> getProductsByPage(int page) {
+		return productDao.getAll(page * Constant.PAGE_NUMBER);
 	}
 
 	public Product getProduct(Long id) {
@@ -36,25 +31,16 @@ public class ProductService {
 		productDao.save(entity);
 	}
 
-	private Specification<Product> buildSpecification(Long userId,
-			Map<String, Object> searchParams) {
-		Map<String, SearchFilter> filters = SearchFilter.parse(searchParams);
-		filters.put("user.id", new SearchFilter("user.id",Operator.EQ, userId));
-		Specification<Product> spec = DynamicSpecifications.bySearchFilter(filters.values(), Product.class);
-		return spec;
-	}
-
-	private PageRequest buildPageRequest(int pageNumber, int pageSize,
-			String sortType) {
+	private PageRequest buildPageRequest(int pageNumber, int pageSize, String sortType) {
 		Sort sort = null;
-		if("auto".equals(sortType)) {
+		if ("auto".equals(sortType)) {
 			sort = new Sort(Direction.DESC, "id");
-		} else if("title".equals(sortType)) {
-			sort = new Sort(Direction.ASC,"title");
+		} else if ("title".equals(sortType)) {
+			sort = new Sort(Direction.ASC, "title");
 		}
 		return new PageRequest(pageNumber - 1, pageSize, sort);
 	}
-	
+
 	@Autowired
 	public void setProductDao(ProductDao productDao) {
 		this.productDao = productDao;
--- trunk/kuguo-front/src/main/java/com/kuguo/front/web/ProductController.java
+++ trunk/kuguo-front/src/main/java/com/kuguo/front/web/ProductController.java
@@ -1,14 +1,11 @@
 package com.kuguo.front.web;
 
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.ServletRequest;
 import javax.validation.Valid;
 
-import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -17,9 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springside.modules.web.Servlets;
 
-import com.google.common.collect.Maps;
 import com.kuguo.front.entity.Channel;
 import com.kuguo.front.entity.Label;
 import com.kuguo.front.entity.Product;
@@ -32,75 +27,74 @@ import com.kuguo.front.service.UserService;
 @Controller
 @RequestMapping(value = "/product")
 public class ProductController {
-	
-	
+
 	@Autowired
 	private ProductService productService;
-	
+
 	@Autowired
 	private ChannelService channelService;
-	
+
 	@Autowired
 	private LabelService labelService;
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@RequestMapping(value = "")
-	public String list(@RequestParam(value = "sortType",defaultValue = "auto") String sortType,
-			@RequestParam(value = "page",defaultValue = "1") int pageNumber, Model model,ServletRequest request) {
+	public String list(@RequestParam(value = "sortType", defaultValue = "auto") String sortType,
+			@RequestParam(value = "page", defaultValue = "1") int pageNumber, Model model, ServletRequest request) {
 		List<Product> products = productService.getProductsByPage(pageNumber);
-		List<Channel> channels = channelService.getAllChannel(); 
+		List<Channel> channels = channelService.getAllChannel();
 		List<Label> labels = labelService.getLabel();
 		List<User> users = userService.getHotUsers(5);
-		model.addAttribute("products",products);
-		model.addAttribute("channels",channels);
+		model.addAttribute("products", products);
+		model.addAttribute("channels", channels);
 		model.addAttribute("labels", labels);
 		model.addAttribute("users", users);
-		
+
 		return "product/productList";
 	}
-	
+
 	@RequestMapping(value = "create", method = RequestMethod.GET)
 	public String createForm(Model model) {
 		model.addAttribute("product", new Product());
 		model.addAttribute("action", "create");
 		return "product/productForm";
 	}
-	
+
 	@RequestMapping(value = "create", method = RequestMethod.POST)
-	public String create(@Valid Product newProduct,RedirectAttributes redirectAttributes) {
+	public String create(@Valid Product newProduct, RedirectAttributes redirectAttributes) {
 		User user = new User();
 		newProduct.setUser(user);
-		
+
 		productService.saveProduct(newProduct);
 		redirectAttributes.addFlashAttribute("message", "创建任务成功");
 		return "redirect:/product/";
 	}
-	
+
 	@RequestMapping(value = "update/{id}", method = RequestMethod.GET)
 	public String updateForm(@PathVariable("id") Long id, Model model) {
-		model.addAttribute("product",productService.getProduct(id));
+		model.addAttribute("product", productService.getProduct(id));
 		model.addAttribute("action", "update");
 		return "product/productForm";
 	}
-	
-	@RequestMapping(value = "update" , method = RequestMethod.POST)
+
+	@RequestMapping(value = "update", method = RequestMethod.POST)
 	public String update(@Valid @ModelAttribute("preloadProduct") Product product, RedirectAttributes redirectAttributes) {
 		productService.saveProduct(product);
 		redirectAttributes.addFlashAttribute("message", "更新任务完成");
 		return "redirect:/product/";
 	}
-	
+
 	@ModelAttribute("preloadProduct")
 	public Product getProduct(@RequestParam(value = "id", required = false) Long id) {
-		if(id != null) {
+		if (id != null) {
 			return productService.getProduct(id);
 		}
 		return null;
 	}
-	
-	@RequestMapping(value = "/taobao/info" , method = RequestMethod.POST)
-	public void getFromTaobao(String url){
+
+	@RequestMapping(value = "/taobao/info", method = RequestMethod.POST)
+	public void getFromTaobao(String url) {
 	}
 }
