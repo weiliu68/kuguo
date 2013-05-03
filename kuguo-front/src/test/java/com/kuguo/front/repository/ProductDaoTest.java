1b86bfa9e3cd2a7143e9dbff2af7a90a091035e3
weiliu68 <weiliu68@2244cf6e-af35-60ac-9df1-d7610207b578>
2013-05-03 07:50:32 +0000

    test

--- kuguo-front/src/test/java/com/kuguo/front/repository/ProductDaoTest.java
+++ kuguo-front/src/test/java/com/kuguo/front/repository/ProductDaoTest.java
@@ -7,9 +7,9 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
-import org.springside.modules.test.spring.SpringTransactionalTestCase;
 import static org.junit.Assert.*;
 
+import com.kuguo.front.SpringTransactionalTestCase;
 import com.kuguo.front.entity.Product;
 
 @DirtiesContext
